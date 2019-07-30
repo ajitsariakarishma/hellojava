3d707f7beda2e172d054709fca0f8dadfe1e72a8
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-07-30 15:25:33 +0100

    gitint git add README.md git commit -m "first commit" git remote add origin https://github.com/ajitsariakarishma/main-details.git git push -u origin master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String name = (" Name : Karishma Ajitsaria");
+        String address = (" Address : 9, Mavendish Drive, Edgware, HA9 0NR");
+        String age = ("Age : 39");
+        String phone_no = ("Phone no : 07810501089" );
+        String salary = ("Salary : $80,000");
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(age);
+        System.out.println(phone_no);
+        System.out.println(salary);
+
     }
 }
