81f67698773c60e6e7dab6813fe89b5e502bf67d
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-07-30 15:21:07 +0100

    trial commit

---
+++ src/trial.java
@@ -0,0 +1,16 @@
+public class trial {
+
+    public static void main(String[] args) {
+        String name = (" Name : Karishma Ajitsaria");
+        String address = (" Address : 9, Mavendish Drive, Edgware, HA9 0NR");
+        int age =  39;
+      String phone_no =  ( "07810501089");
+        int salary = 80000;
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(age);
+        System.out.println(phone_no);
+        System.out.println(salary);
+        }
+        }
+
