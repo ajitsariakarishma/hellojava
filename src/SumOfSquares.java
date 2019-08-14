4686d1512572d178a6f0e111c0af7b9a1805e60e
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-08-14 17:44:37 +0100

    first commit

--- src/SumOfSquares.java
+++ src/SumOfSquares.java
@@ -5,15 +5,19 @@ public static void main(String[] args){
     int i =1;
    int sum = 0;
    int sum2 = 0;
+   int temp1=0;
     do{
-        sum = i*i*i;
-        int temp = sum;
+        sum =(i*i)+(i*i);
+         temp1=i*i;
+         temp1 = sum;
         sum2=sum2+sum;
+        System.out.println("The sum of square of "+i+" is :"+sum );
         i++;
 
 
     }while (i<=100);
-    System.out.print("The sum of cubes of all numbers from 1 to 100 is:   "+sum2+"  ");
+    System.out.print("The sum of squares of all numbers from 1 to 100 is:   "+sum2+"  ");
+
 
 }
 
