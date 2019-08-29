828277d7b741b07512f4fec8ea72ad93c12588b1
ajitsariakarishma <karishmaajitsaria@gmail.com>
2019-08-29 20:09:22 +0100

    first commit

--- src/FactorialNumber.java
+++ src/FactorialNumber.java
@@ -13,11 +13,21 @@ public class FactorialNumber {
 
 
     }
+    //Recursive function
+    public static int RecursiveFacorial(int num) {
+
+        if (num == 1) {
+            return 1;
+        } else {
+            return (num * RecursiveFacorial(num - 1));
+
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Factorial of 4 is :"+IsFactorial(4));
         System.out.println("Factorial of 10 is :"+IsFactorial(10));
         System.out.println("Factorial of 5 is :"+IsFactorial(5));
-
+        System.out.println("factorial of 4 is :"+ RecursiveFacorial(4));
     }
 }
